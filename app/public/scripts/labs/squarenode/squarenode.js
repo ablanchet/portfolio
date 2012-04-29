fe9639e4e31f01600a51112405e8d2e1d4163661
Antoine Blanchet <antoine.blanchet@gmail.com>
2012-04-29 03:08:23 +0200

    squarenode is working, the code is not as clean as I want but it's a first step

--- app/public/scripts/labs/squarenode/squarenode.js
+++ app/public/scripts/labs/squarenode/squarenode.js
@@ -2,21 +2,62 @@
 
     var _connection;
 
+    var _wrapper;
+    var _zindex = 1;
+
     var init = function () {
         _connection.on('message', function (data) {
             console.log(data);
-        });
-
-        _connection.on('move', function (square) {
-            move(square);
+        })
+        .on('new-square', function (data) {
+            addSquare(data.square, data.ownership);
+        })
+        .on('move', function (data) {
+            move(data.id, { left: data.x, top: data.y });
+        })
+        .on('delete', function (data) {
+            deleteSquare(data.id);
         });
     };
 
-    var move = function (square) {
+    var addSquare = function (square, ownership) {
+        var sq = $('<div class="square" data-id="' + square.id + '" style="background: rgb(' + square.color.r + ',' + square.color.g + ',' + square.color.b + ');"></div>');
+        sq.css({
+            left: square.pos.x,
+            top: square.pos.y,
+            display: 'none'
+        });
+        if (ownership) {
+            sq.addClass('mine').append('<p>Your square !</p>');
+
+            sq.bind('drag', function (event) {
+                var pos = {
+                    top: event.offsetY - 79,
+                    left: event.offsetX - 260
+                };
+                _connection.emit('move', { y: pos.top, x: pos.left });
+                $(this).css(pos);
+            });
+        }
+        else sq.css('z-index', _zindex++);
+
+        _wrapper.append(sq);
+        sq.fadeIn();
+    };
+
+    var move = function (squareId, pos) {
+        $('[data-id=' + squareId + ']').css(pos);
+    };
+
+    var deleteSquare = function (squareId) {
+        $('[data-id=' + squareId + ']').fadeOut(function () {
+            $(this).detach();
+        });
     };
 
     $(function () {
         _connection = io.connect('http://' + window.location.host + '/square');
+        _wrapper = $('#squares');
         init();
     });
 
--- app/squarenode.js
+++ app/squarenode.js
@@ -7,15 +7,63 @@ var io = require('socket.io');
 // SquareNode
 
 var _socket;
-var _clients = [];
+var _clients = {};
 
 var init = function (socket) {
     _socket = socket.of('/square');
+    _socket.on('connection', onNewClient);
+};
 
-    _socket.on('connection', function (client) {
-        _clients.push(client);
-        client.emit('message', 'Welcome !');
+var onNewClient = function (client) {
+    // bind its deconnection
+    client.on('disconnect', function () {
+        delete _clients[client.id];
+        _socket.emit('delete', { id: client.id });
     });
+
+    client.on('move', function (data) {
+        onMove(data, client);
+    });
+
+    // create its square and send it to him
+    var sq = createSquare(client);
+    client.emit('new-square', { square: sq, ownership: true });
+
+    iterateClients(function (c) {
+        // send the new square to other clients
+        c.client.emit('new-square', { square: sq });
+        // send all the other squares to the new client
+        client.emit('new-square', { square: c.square });
+    });
+
+    // add the square to the clients
+    _clients[client.id] = { square: sq, client: client };
+};
+
+var onMove = function (data, client) {
+    // update square position
+    _clients[client.id].square.pos = data;
+    iterateClients(function (c) {
+        c.client.emit('move', { id: client.id, x: data.x, y: data.y });
+    }, client);
+};
+
+var createSquare = function(client){
+    var color = { 
+        r: Math.round( Math.random() * 100 % 255 ), 
+        g: Math.round( Math.random() * 100 % 255 ),
+        b: Math.round( Math.random() * 100 % 255 )
+    };
+    var pos = { x: 0, y: 0 };
+    return { color: color, pos: pos, id: client.id };
+};
+var iterateClients = function (action, except) {
+    for (var c in _clients) {
+        var client = _clients[c];
+        if (except == null || client.id != except.id) {
+            action(client);
+        }
+    }
 };
 
 module.exports.start = function (expressServer) {
