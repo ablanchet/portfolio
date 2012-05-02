3200e155b267b58788f2e63f6b1810fd065f5b4e
Antoine Blanchet <antoine.blanchet@gmail.com>
2012-05-03 01:03:10 +0200

    update fonts and parallax module

--- app/public/scripts/labs/parallax/parallax.js
+++ app/public/scripts/labs/parallax/parallax.js
@@ -1,53 +1,68 @@
 (function () {
 
-    var _contents, _bg;
+    var _contents, _bg, _itemWidth;
+    var _currentItemPlaceHolder;
 
     var _itemsCount;
     var _currentItemId = 0;
-    var _itemWidth = 630;
-    var _parallaxRatio = 0.1;
+    var _parallaxRatio = 13 / 100;
 
-    var goLeft = function () {
+    var _sizeOfLeftElementForNavigation = 50;
+
+    var goRigth = function () {
         if (_currentItemId + 1 < _itemsCount) {
             _currentItemId++;
             move();
         }
     };
-
-    var goRigth = function () {
+    var goLeft = function () {
         if (_currentItemId > 0) {
             _currentItemId--;
             move();
         }
     };
-
     var goTo = function () {
-        _currentItemId = $(this).data('id');
-        move();
+        var id = $(this).data('id');
+        if (_currentItemId != id) {
+            _currentItemId = id;
+            move();
+        }
     };
 
     var move = function () {
         var positions = computePositions();
         _contents.css('left', positions.elements + 'px');
         _bg.css('left', positions.background + 'px');
+        
+        refreshCurrentItemPlaceHolder();
     };
-
     var computePositions = function () {
         var elements = (0 - _currentItemId) * _itemWidth;
+        if (_currentItemId != 0) elements += _sizeOfLeftElementForNavigation;
         var background = elements * _parallaxRatio;
 
         return { elements: elements, background: background };
     };
 
+    var refreshCurrentItemPlaceHolder = function () {
+        _currentItemPlaceHolder.html(_currentItemId + 1 + '/' + _itemsCount);
+    };
+
     $(function () {
         $('.goleft').click(goLeft);
         $('.gorigth').click(goRigth);
+
         $('.content').click(goTo);
 
-        _contents = $('.contents');
-        _itemsCount = $('.content').length;
-        _contents.css('width', (_itemWidth * _itemsCount) + 'px');
+        _currentItemPlaceHolder = $('.currentItem');
         _bg = $('.parallax-bg');
+        _contents = $('.contents');
+        var content = $('.content', _contents);
+        _itemWidth = content.outerWidth(true);
+        _itemsCount = content.length;
+        _contents.css('width', (_itemWidth * _itemsCount) + 'px');
+
+        refreshCurrentItemPlaceHolder();
     });
 
 } ());
