888188e272a4d4e3fa0ccb0bacd0b499573d3495
Antoine Blanchet <antoine.blanchet@gmail.com>
2012-05-14 00:01:48 +0200

    bind hover events on items and allow to stop the animation

--- app/public/scripts/labs/infinitescroll/infinitescroll.js
+++ app/public/scripts/labs/infinitescroll/infinitescroll.js
@@ -1,25 +1,53 @@
 (function () {
     var _pagewidth = 3960;
-    var _animationduration = 20000;
+    var _durationratio = 3;
     var _container;
 
-    var _animatePage = function (first, second, third) {
-        var a = first.animate({ left: -_pagewidth }, _animationduration, 'linear', function () {
+    var _getPages = function () {
+        return _container.children('.page');
+    };
+
+    var _bindEvents = function (elements) {
+        elements.hover(function () {
+            _getPages().stop();
+        },
+        function () {
+            _animatePages();
+        })
+        .children('.item').hover(function () {
+            $(this).animate({ width: 600 }, 500);
+        },
+        function () {
+            $(this).clearQueue();
+            $(this).animate({ width: 300 }, 500);
+        });
+    };
+
+    var _animatePages = function () {
+        var pages = _getPages();
+
+        var p1 = $(pages[0]);
+        var p2 = $(pages[1]);
+        var p3 = $(pages[2]);
+
+        var current = p1.position().left;
+        var distance = _pagewidth - Math.abs(current);
+        p1.animate({ left: -_pagewidth }, distance * _durationratio, 'linear', function () {
             var el = $(this).clone();
             $(this).detach();
             el.css('left', '7920px');
             _container.append(el);
-            _animatePage(second, third, el);
+            _animatePages();
+            _bindEvents(el);
         });
-        second.animate({ left: 0 }, _animationduration, 'linear');
-        third.animate({ left: _pagewidth }, _animationduration, 'linear');
-
-        console.log(a);
+        p2.animate({ left: 0 }, distance * _durationratio, 'linear');
+        p3.animate({ left: _pagewidth }, distance * _durationratio, 'linear');
     };
 
     var basicAnimation = function () {
         _container = $('.container');
-        _animatePage($('.p1'), $('.p2'), $('.p3'));
+        _animatePages();
+        _bindEvents(_getPages());
     };
 
     $(function () {
