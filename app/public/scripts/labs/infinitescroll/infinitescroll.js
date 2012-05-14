f1b9348b1ac0a2a1a0e6c9cb6394442c3387951a
Antoine Blanchet <antoine.blanchet@gmail.com>
2012-05-14 15:55:50 +0200

    allow infinitescroll to change direction

--- app/public/scripts/labs/infinitescroll/infinitescroll.js
+++ app/public/scripts/labs/infinitescroll/infinitescroll.js
@@ -1,10 +1,21 @@
 (function () {
+    var _directions = { left: -1, right: +1 };
+    var _currentdirection = _directions.right;
+
     var _pagewidth = 3960;
-    var _durationratio = 3;
+    var _durationratio = 10;
     var _container;
 
     var _getPages = function () {
         return _container.children('.page');
+    };
+
+    var _changeDirection = function (direction) {
+        if (direction != _currentdirection) {
+            _currentdirection = direction;
+            _getPages().stop();
+            _animatePages();
+        }
     };
 
     var _bindEvents = function (elements) {
@@ -13,13 +24,6 @@
         },
         function () {
             _animatePages();
-        })
-        .children('.item').hover(function () {
-            $(this).animate({ width: 600 }, 500);
-        },
-        function () {
-            $(this).clearQueue();
-            $(this).animate({ width: 300 }, 500);
         });
     };
 
@@ -29,25 +33,42 @@
         var p1 = $(pages[0]);
         var p2 = $(pages[1]);
         var p3 = $(pages[2]);
+        var current = p2.position().left;
 
-        var current = p1.position().left;
-        var distance = _pagewidth - Math.abs(current);
-        p1.animate({ left: -_pagewidth }, distance * _durationratio, 'linear', function () {
-            var el = $(this).clone();
-            $(this).detach();
-            el.css('left', '7920px');
-            _container.append(el);
-            _animatePages();
-            _bindEvents(el);
-        });
-        p2.animate({ left: 0 }, distance * _durationratio, 'linear');
-        p3.animate({ left: _pagewidth }, distance * _durationratio, 'linear');
+        if (_currentdirection == _directions.right) {
+            var distance = _pagewidth - current;
+            p1.animate({ left: 0 }, distance * _durationratio, 'linear');
+            p2.animate({ left: _pagewidth }, distance * _durationratio, 'linear', function () {
+                var el = p3.clone();
+                p3.detach();
+                el.css('left', '-3960px');
+                _container.prepend(el);
+                _animatePages();
+                _bindEvents(el);
+            });
+            p3.animate({ left: _pagewidth * 2 }, distance * _durationratio, 'linear');
+        }
+        else { // the left direction is the default
+            var distance = _pagewidth + current;
+            p1.animate({ left: -(_pagewidth * 2) }, distance * _durationratio, 'linear');
+            p2.animate({ left: -_pagewidth }, distance * _durationratio, 'linear', function () {
+                var el = p1.clone();
+                p1.detach();
+                el.css('left', '3960px');
+                _container.append(el);
+                _animatePages();
+                _bindEvents(el);
+            });
+            p3.animate({ left: 0 }, distance * _durationratio, 'linear');
+        }
     };
 
     var basicAnimation = function () {
         _container = $('.container');
         _animatePages();
         _bindEvents(_getPages());
+        $('.navleft').hover(function () { _changeDirection(_directions.right); });
+        $('.navright').hover(function () { _changeDirection(_directions.left); });
     };
 
     $(function () {
